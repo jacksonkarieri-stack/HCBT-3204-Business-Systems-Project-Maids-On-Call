8b674049af3fe4ab407460d9a37b145feed30cb6
Jackson-Karieri <jackson.karieri@students.jkuat.ac.ke>
2025-09-07 10:42:46 +0300

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,17 @@
+How to run the Maids On Call Project
+
+
+3.Paste inside root directory(for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/HTML)
+
+4.Open PHPMyAdmin (http://localhost or 127.0.0.1/phpmyadmin)
+
+5. Create a database with the name mocdb
+
+6. Import mocdb.sql file(given inside the zip package in the SQL file folder)
+
+7. Run the script http://localhost/moc
+
+Credential for Admin panel :
+
+Username: admin
+Password: Test@123
---
+++ maid-hiring.php
@@ -0,0 +1,319 @@
+
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+   
+     <title>Maids On Call || Request Form</title>
+    
+
+   <!-- CSS here -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/slicknav.css">
+        <link rel="stylesheet" href="assets/css/price_rangs.css">
+        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+        <link rel="stylesheet" href="assets/css/themify-icons.css">
+        <link rel="stylesheet" href="assets/css/themify-icons.css">
+        <link rel="stylesheet" href="assets/css/slick.css">
+        <link rel="stylesheet" href="assets/css/nice-select.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+     <!-- Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <strong>Maids On Call</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader Start -->
+    <header>
+        <!-- Header Start -->
+       <div class="header-area header-transparrent">
+           <div class="headder-top header-sticky">
+                <div class="container">
+                    <div class="row align-items-center mb-5">
+                        <div class="col-lg-3 col-md-2">
+                            <!-- Logo -->
+                             
+                        </div>
+                        <div class="col-lg-9 col-md-9">
+                            <div class="menu-wrapper">
+                               <!-- Main-menu -->
+                                <div class="main-menu">
+                                    <nav class="d-none d-lg-block">
+                                        <ul id="navigation">
+                                            <li><a href="index.php">Home</a></li>
+                                            <li><a href="maid-hiring.php">Request Form </a></li>
+                                            <li><a href="about.php">About Us</a></li>
+                                           
+                                            <li><a href="contact.php">Contact Us</a></li>
+                                            <li><a href="admin/login.php">Admin Portal</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>          
+                                <!-- Header-btn -->
+                               
+                            </div>
+                        </div>
+                        <!-- Mobile Menu -->
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+       </div>
+        <!-- Header End -->
+    </header>    <!-- Hero Area Start-->
+    <div class="slider-area ">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hero-cap text-center">
+                            <h2>Maid Request Form</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <!-- Hero Area End -->
+    <!-- ================ contact section start ================= -->
+    <section class="contact-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <p class="contact-title" style="text-left: center;color: green;">Kindly Fill the Details ></p>
+                    </div>
+                    <div class="col-lg-8" style="padding-top=20px">
+
+                        <form class="form-contact contact_form" action="" method="post">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Your Name:</label>
+                                        <input class="form-control" name="name" id="name" type="text" placeholder="Enter your name">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Contact Number:</label>
+                                        <input type="text" name="contno" value="" class="form-control" required='true' maxlength="10" pattern="[0-9]+" placeholder="+2547">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Your Address:</label>
+                                        <textarea class="form-control" name="address" id="address" placeholder="Enter Your Addresss"></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px; padding-left: 20px;">Gender:</label>
+                                       <select name="gender" class="form-control" required='true'>
+                                 <option value="">Select Gender</option>
+                                 <option value="Female">Female</option>
+                                 <option value="Male">Male</option>
+                                 <option value="NotToAnswer">Not to Answer</option>
+                             </select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Email Address:</label>
+                                        <input class="form-control" name="email" id="email" type="email" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Your Location/County:</label>
+                                        <input class="form-control" name="county" id="county" type="county" placeholder="county">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;padding-left: 20px;">Select Service:</label>
+                                       <select name="catid" class="form-control" required='true'>
+                                 <option value="">Select Service</option>
+                                    
+   
+<option value="1">Dusting</option>
+   
+   
+<option value="2">Mopping</option>
+   
+   
+<option value="3">Dusting and Mopping</option>
+   
+   
+<option value="4">Utensil Cleaning</option>
+   
+   
+<option value="5">Toilet Cleaning</option>
+   
+   
+<option value="6">Dusting, Utensil and Mopping</option>
+   
+   
+<option value="8">Others</option>
+ </select>
+                                    </div>
+                                </div>
+                              
+                                <div class="col-sm-12" style="padding-top: 20px;">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Work Shift From:</label>
+                                        <input class="form-control" name="wsf" id="wsf" type="time">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Work Shift To:</label>
+                                        <input class="form-control" name="wst" id="wst" type="time">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Start Date:</label>
+                                        <input class="form-control" name="startdate" id="startdate" type="date">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <label style="color: red;font-weight: bold;font-size: 20px;">Notes:</label>
+                                        <textarea class="form-control" name="notes" id="notes" type="text" placeholder="Enter Some Notes"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group mt-3">
+                                <button type="submit" class="button button-contactForm boxed-btn" name="submit">Send</button>
+                            </div>
+                        </form>
+                    </div>
+                 
+                </div>
+            </div>
+        </section>
+    <!-- ================ contact section end ================= -->
+  <footer>
+        <!-- Footer Start-->
+        <div class="footer-area footer-bg footer-padding">
+            <div class="container">
+                <div class="row d-flex justify-content-between">
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
+                       <div class="single-footer-caption mb-50">
+                         <div class="single-footer-caption mb-30">
+                             <div class="footer-tittle">
+                                                                 <h4>About Us</h4>
+                                 <div class="footer-pera">
+                                     <p>Mafua Management System
+.</p>                                </div>
+                             </div>
+                         </div>
+
+                       </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-tittle">
+                                <h4>Contact Info</h4>
+                                <ul>
+                                                                        <li>
+                                    <p>Address :1 - 90150, Nairobi</p>
+                                    </li>
+                                    <li><a href="#">Phone : 727297380</a></li>
+                                    <li><a href="#">Email : mamafua-help@hotmail.com</a></li>                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
+                        <div class="single-footer-caption mb-50">
+                            <div class="footer-tittle">
+                                <h4>Important Link</h4>
+                                <ul>
+                                    <li><a href="index.php"> Home</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="maid-hiring.php">Maid Hiring</a></li>
+                                    <li><a href="admin/login.php">Admin</a></li>
+                                   
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+               <!--  -->
+              
+            </div>
+        </div>
+        <!-- footer-bottom area -->
+        <div class="footer-bottom-area footer-bg">
+            <div class="container">
+                <div class="footer-border">
+                     <div class="row d-flex justify-content-between align-items-center">
+                         <div class="col-xl-10 col-lg-10 ">
+                             <div class="footer-copy-right">
+                                 <p>
+   Maids On Call<i class="fa fa-heart" aria-hidden="true"></i>
+ </p>
+                             </div>
+                         </div>
+                   
+                     </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End-->
+    </footer><!-- JS here -->
+	
+		<!-- All JS Custom Plugins Link Here here -->
+        <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+		<!-- Jquery, Popper, Bootstrap -->
+		<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="./assets/js/popper.min.js"></script>
+        <script src="./assets/js/bootstrap.min.js"></script>
+	    <!-- Jquery Mobile Menu -->
+        <script src="./assets/js/jquery.slicknav.min.js"></script>
+
+		<!-- Jquery Slick , Owl-Carousel Plugins -->
+        <script src="./assets/js/owl.carousel.min.js"></script>
+        <script src="./assets/js/slick.min.js"></script>
+        <script src="./assets/js/price_rangs.js"></script>
+        
+		<!-- One Page, Animated-HeadLin -->
+        <script src="./assets/js/wow.min.js"></script>
+		<script src="./assets/js/animated.headline.js"></script>
+		
+		<!-- Scrollup, nice-select, sticky -->
+        <script src="./assets/js/jquery.scrollUp.min.js"></script>
+        <script src="./assets/js/jquery.nice-select.min.js"></script>
+		<script src="./assets/js/jquery.sticky.js"></script>
+        <script src="./assets/js/jquery.magnific-popup.js"></script>
+
+        <!-- contact js -->
+        <script src="./assets/js/contact.js"></script>
+        <script src="./assets/js/jquery.form.js"></script>
+        <script src="./assets/js/jquery.validate.min.js"></script>
+        <script src="./assets/js/mail-script.js"></script>
+        <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+        
+		<!-- Jquery Plugins, main Jquery -->	
+        <script src="./assets/js/plugins.js"></script>
+        <script src="./assets/js/main.js"></script>
+        
+    </body>
+    
+    </html>
